3e629ee42bc909c5b126320e386e925c805b967b
Donal Murphy <domo232@hotmail.com>
2023-06-28 17:36:02 +0100

    Exercise - Show favourite icon on Movie Details page

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -5,6 +5,8 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Avatar from "@mui/material/Avatar";
 
 const styles = {
     root: {  
@@ -13,17 +15,35 @@ const styles = {
     alignItems: "center",
     flexWrap: "wrap",
     padding: 1.5,
+    avatar: {
+      backgroundColor: "rgb(255, 0, 0)",
+    }
   },
 };
 
 const MovieHeader = (props) => {
   const movie = props.movie;
-
+  const movies = JSON.parse(localStorage.getItem("favourites"));
+  
+  for (var i = 0; i <= movies.length - 1; i++) {
+      if (movie.id == movies[i].id){
+        var flag = true;    
+      }
+  }
+  
   return (
     <Paper component="div" sx={styles.root}>
       <IconButton aria-label="go back">
         <ArrowBackIcon color="primary" fontSize="large" />
       </IconButton>
+
+    <div>
+      {flag ? (
+        <Avatar sx={styles.root.avatar}>
+          <FavoriteIcon />
+        </Avatar>  
+      ) : null}
+    </div>
 
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
@@ -33,6 +53,7 @@ const MovieHeader = (props) => {
         <br />
         <span>{`${movie.tagline}`} </span>
       </Typography>
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
